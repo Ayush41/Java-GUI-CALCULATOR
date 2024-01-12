1ab5d5b5154977b3131076d3f0f6fe9701e913dd
ayush41 <bhattayushraj@gmail.com>
2024-01-12 20:10:17 +0530

    func

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -2,3 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+//to extend class swing:)
+
+
+public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> {
+        CalculatorApp calculatorApp = new CalculatorApp();
+        calculatorApp.setVisible(true);
+    });
+}
